9742db9bb25c1d215ace328cf4dd06d55f9c49ab
ankit <ankit@cse.iitb.ac.in>
2017-10-02 05:50:58 +0530

    change1 many more to come

--- app/src/main/java/com/example/ankit/insticable/MainActivity.java
+++ app/src/main/java/com/example/ankit/insticable/MainActivity.java
@@ -42,7 +42,6 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.udacity.friendlychat.R;
 
 import java.util.ArrayList;
 import java.util.Arrays;
